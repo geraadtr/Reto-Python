27851185011cf93ce5caa3500312198cd1133dea
Gerardo G <159053565+geraadtr@users.noreply.github.com>
2024-02-05 21:53:59 -0600

    Add files via upload
    Reto del día 1, muchas gracias por el aprendizaje y la retroalimentación.

---
+++ RetoPhyton1.py
@@ -0,0 +1,6 @@
+nombre = input("Por favor, escribe tu nombre:")
+apellido = input("Por favor, escribe tu apellido:")
+nombre_completo = nombre + " " + apellido
+telefono = input("Por favor, escribe tu número telefónico:")
+correo_electronico = input("Por favor, escribe tu correo electronico:")
+print(f"Hola {nombre_completo}, en breve recibirás un correo a {correo_electronico}.")
